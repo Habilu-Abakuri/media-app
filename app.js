6f9afb1fe0beea64234551cf9a4f622203187b4c
Habilu-Abakuri <info2habilu@gmail.com>
2022-11-07 23:22:01 +0100

    edit and delete functions addition

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ form.addEventListener("submit", (e) => {
     e.preventDefault();
 
     formValidation();
-})
+});
 
 let formValidation = () => {
     if (input.value === "") {
@@ -20,7 +20,7 @@ let formValidation = () => {
     }
 };
 
-//getting the enter data
+//getting the entered data
 let data = {};
 
 let acceptData = () => {
@@ -29,15 +29,23 @@ let acceptData = () => {
 };
 
 let createPost = () => {
-    post.innerHTML += 
+    post.innerHTML += `
     <div>
-        <p>${data.text}</p>
-        <span class="options">
-            <i onClick="editPost(this)" class="fas fa-edit"></i>
-            <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
-        </span>
-    </div>;
-
+      <p>${data.text}</p>
+      <span class="options">
+        <i onClick="editPost(this)" class="fas fa-edit"></i>
+        <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+      </span>
+    </div>
+    `;
     input.value = "";
+};  
+
+let deletePost = (e) => {
+    e.parentElement.parentElement.remove();
 };
 
+let editPost = (e) => {
+    input.value = e.parentElement.previousElementSibling.innerHTML;
+    e.parentElement.parentElement.remove();
+}
